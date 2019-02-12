8b063eef3b1d93dab9b48c591252eb9b5a48bbfe
zyy <zyy_13579@163.com>
2019-02-12 11:50:11 +0800

    完成登录登出功能

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,8 +8,8 @@ public func routes(_ router: Router) throws {
     let session = User.authSessionsMiddleware()
     let auth = router.grouped(session)
     // Basic "It works" example
-    router.get { req in
-        return "It works!"
+    auth.get { req -> Response in
+        return req.redirect(to: "/admin")
     }
     
     router.get("login") { req -> Future<View> in
@@ -22,21 +22,40 @@ public func routes(_ router: Router) throws {
         return "Hello, world, \(user.username)!"
     }
     
-    auth.post("logon") { req -> EventLoopFuture<EventLoopFuture<EventLoopFuture<View>>> in
-        return try req.content.decode(LoginRequest.self).map{loginRequest -> EventLoopFuture<EventLoopFuture<View>> in
+    auth.post("logon") { req -> EventLoopFuture<EventLoopFuture<Response>> in
+        return try req.content.decode(LoginRequest.self).map{loginRequest -> EventLoopFuture<Response> in
             let lr = loginRequest
             return User.query(on: req).filter(\.username == lr.username).first().map{ user in
                 if user == nil{
-                    return try req.view().render("login", ["reason": "没有该用户"])
+//                    return try req.view().render("login", ["reason": "没有该用户"])
+                    return req.redirect(to: "/login?reason=没有该用户")
                 }
                 let u = user!
                 let result = try BCrypt.verify(lr.password, created: u.password)
                 if result == false{
-                    return try req.view().render("login", ["reason": "用户名密码不正确"])
+//                    return try req.view().render("login", ["reason": "用户名密码不正确"])
+                    return req.redirect(to: "/login?reason=用户名密码不正确")
                 }
                 try req.authenticate(u)
-                return try req.view().render("index", ["name": u.username])
+//                return try req.view().render("admin", ["user": u])
+                return req.redirect(to: "/admin")
             }
+        }
+    }
+    
+    auth.get("logout") { req -> Future<View> in
+        try req.destroySession()
+        return try req.view().render("login", ["msg": "已经退出"])
+    }
+    
+    auth.get("admin") { req -> Future<View> in
+        let islogon = try req.isAuthenticated(User.self)
+        if islogon == true {
+            let user = try req.requireAuthenticated(User.self)
+            return try req.view().render("admin", ["user": user])
+        }
+        else{
+            return try req.view().render("login", ["name": "Leaf"])
         }
     }
 
