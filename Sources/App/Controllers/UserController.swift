e1009a10ef7daea51d81dc3d349eb717ec782838
zyy <zyy_13579@163.com>
2019-02-12 10:31:48 +0800

    完成密码加密

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,33 @@
+//
+//  UserController.swift
+//  App
+//
+//  Created by 赵友源 on 2019/2/12.
+//
+
+import Vapor
+import Crypto
+
+/// Controls basic CRUD operations on `Todo`s.
+final class UserController {
+    /// Returns a list of all `Todo`s.
+    func index(_ req: Request) throws -> Future<[User]> {
+        return User.query(on: req).all()
+    }
+    
+    /// Saves a decoded `Todo` to the database.
+    func create(_ req: Request) throws -> Future<User> {
+        return try req.content.decode(User.self).flatMap { user in
+            let hashpw = try BCrypt.hash(user.password, cost: 4)
+            user.password = hashpw
+            return user.save(on: req)
+        }
+    }
+    
+    /// Deletes a parameterized `Todo`.
+    func delete(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(User.self).flatMap { user in
+            return user.delete(on: req)
+            }.transform(to: .ok)
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,7 @@
 import Vapor
+import Crypto
+import Random
+import Fluent
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -19,21 +22,21 @@ public func routes(_ router: Router) throws {
         return "Hello, world, \(user.username)!"
     }
     
-    auth.post("logon") { req -> EventLoopFuture<EventLoopFuture<View>> in
-//        let result = try req.content.decode(LoginRequest.self).wait()
-//        print(result)
-//        let user = try User.query(on: req).first().wait()
-//        if user == nil {
-//            return try req.view().render("login", ["reason": "没有该用户"])
-//        }
-//        try req.authenticate(user!)
-//        return try req.view().render("index", ["name": user?.username])
-        return User.query(on: req).first().map{user in
-            if user == nil{
-                return try req.view().render("login", ["reason": "没有该用户"])
+    auth.post("logon") { req -> EventLoopFuture<EventLoopFuture<EventLoopFuture<View>>> in
+        return try req.content.decode(LoginRequest.self).map{loginRequest -> EventLoopFuture<EventLoopFuture<View>> in
+            let lr = loginRequest
+            return User.query(on: req).filter(\.username == lr.username).first().map{ user in
+                if user == nil{
+                    return try req.view().render("login", ["reason": "没有该用户"])
+                }
+                let u = user!
+                let result = try BCrypt.verify(lr.password, created: u.password)
+                if result == false{
+                    return try req.view().render("login", ["reason": "用户名密码不正确"])
+                }
+                try req.authenticate(u)
+                return try req.view().render("index", ["name": u.username])
             }
-            try req.authenticate(user!)
-            return try req.view().render("index", ["name": user?.username])
         }
     }
 
@@ -42,4 +45,9 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    let userController = UserController()
+    router.get("users", use: userController.index)
+    router.post("users", use: userController.create)
+    router.delete("users", User.parameter, use: userController.delete)
 }
